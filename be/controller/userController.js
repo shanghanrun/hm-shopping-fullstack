3ade6dddabcd58ce41b7d6de4bcd4d0a10d86818
shanghanrun <idim7@naver.com>
2024-05-02 01:09:58 +0900

    .

--- be/controller/userController.js
+++ be/controller/userController.js
@@ -25,22 +25,20 @@ userController.createUser = async(req, res)=>{
 userController.loginWithEmail= async(req, res)=>{
 	try{
 		const {email,password} = req.body;
-		const user = await User.findOne({email}, {
-			createdAt:0, updatedAt:0, __v:0
-		})
-		console.log('찾은 유저 정보 :', user )
+		const user = await User.findOne({email})
+		// console.log('찾은 유저 정보 :', user )
 		if(!user){
 			// 가입한 상태가 아니라는 메시지, 로그인페이지로 리디렉션
 			throw new Error('가입한 상태가 아닙니다. email을 다시 확인해 주세요')
 		} else{
 			const isMatch = bcrypt.compareSync(password, user.password);  //user.password는 암호화된 것
 			if(!isMatch){
-				//암호가 일치하지 않습니다. 정확한 암호를 입력해주세요.
-				// 로그인페이지로 다시 리디렉션
 				throw new Error('패스워드가 일치하지 않습니다.')
+				// 프론트앤드에서는 status400이면 로그인다시하도록
 			} else{
 				//1. 토큰 생성 2. 유저정보와 토큰을 보냄 3.홈페이지로 이동 
-				const token =user.generateToken()
+				const token =await user.generateToken()
+				console.log('로그인 성공')
 				return res.status(200).json({status:'success', user, token})
 			}
 		}
--- be/model/User.js
+++ be/model/User.js
@@ -1,5 +1,9 @@
 const mongoose =require('mongoose')
 const Schema = mongoose.Schema
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+const secretKey = process.env.JWT_SECRET
+
 const userSchema = Schema({
 	email:{type:String, required:true, unique:true},
 	password:{type:String, required:true},
@@ -14,6 +18,10 @@ userSchema.methods.toJSON =function(){
 	delete obj.createdAt
 	delete obj.updatedAt
 	return obj
+}
+userSchema.methods.generateToken = async function(){
+	const token = await jwt.sign({_id:this.id}, secretKey,{expiresIn:"1d"})
+	return token;
 }
 
 const User = mongoose.model("User", userSchema)
